db6232034ae3c6fb05e05e71e45c40605ebaed28
Artem.Tkach <art.tkach@gmail.com>
2026-04-19 21:32:36 +0300

    add test containers

--- src/main/java/com/ta/orders/mappers/ProductCategoryMapper.java
+++ src/main/java/com/ta/orders/mappers/ProductCategoryMapper.java
@@ -3,9 +3,11 @@ package com.ta.orders.mappers;
 import com.ta.orders.dto.ProductCategoryDto;
 import com.ta.orders.model.ProductCategory;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductCategoryMapper {
+    @Mapping(target = "creationTime", ignore = true)
     ProductCategory toEntity(ProductCategoryDto dto);
     ProductCategoryDto toDto(ProductCategory entity);
 }
